0c9456b170c3f62613519c404d89681b467d04d2
Bibiana Cristofol Amat <bcristofol@gmail.com>
2026-05-26 09:53:41 +0100

    src: components: publishers: parts: FileSummary fix rounding and spacing of Total Amount

--- src/components/publishers/parts/FileSummary.vue
+++ src/components/publishers/parts/FileSummary.vue
@@ -19,8 +19,7 @@
     </div>
     <div class="grid__1 align-right" >
       <template v-if="file.aggregate">
-        <span class="file-summary__label">Total amount &pound;</span>
-        {{ (file.aggregate.currencies.GBP.total_amount).toLocaleString() }}
+        <span class="file-summary__stat">£{{ file.aggregate.currencies.GBP.total_amount|numeral("0.0a")}}</span>
         <span class="file-summary__stat">{{ file.grant_total_shorthand }}</span>
       </template>
     </div>
